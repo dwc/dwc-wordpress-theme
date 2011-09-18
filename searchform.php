10219feef6991978e093dfb40031e0c87487241b
Daniel Westermann-Clark <dwc@pobox.com>
2011-09-18 07:08:03 -0700

    Fill search field with query and move magnifying glass icon to left to make room for browser-generated clear button

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" action="<?php esc_attr_e( home_url( '/' ) ); ?>" role="search">
-	<input type="search" name="s" placeholder="Search the site">
+	<input type="search" name="s" placeholder="Search the site"<?php if ( get_query_var( 's' ) ) : ?> value="<?php esc_attr_e( get_query_var( 's' ) ); ?>"<?php endif; ?>>
 	<input type="submit" value="Go">
 </form>
