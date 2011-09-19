af94f463e1fcef3bb3689bd3c7bf39b397da5f19
Daniel Westermann-Clark <dwc@pobox.com>
2011-09-18 21:57:47 -0700

    Make the WP_Query calls easier to update

--- front-page.php
+++ front-page.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
 
-<?php $articles = new WP_Query( 'posts_per_page=2' ); ?>
+<?php
+$articles = new WP_Query( array(
+    'posts_per_page' => 2,
+) );
+?>
 <?php if ( $articles->have_posts() ) : ?>
 	<section id="blog">
 		<h1><a href="<?php dwc_page_link( 'blog' ); ?>">Articles</a></h1>
@@ -12,7 +16,13 @@
 
 <hr>
 
-<?php $projects = new WP_Query( 'post_type=dwc_portfolio&tag=featured&posts_per_page=3' ); ?>
+<?php
+$projects = new WP_Query( array(
+    'post_type' => 'dwc_portfolio',
+    'tag' => 'featured',
+    'posts_per_page' => 3,
+) );
+?>
 <?php if ( $projects->have_posts() ) : ?>
 	<section id="projects">
 		<h1><a href="<?php dwc_page_link( 'portfolio' ); ?>">Featured Projects</a></h1>
