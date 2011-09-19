87ed74c5ccd67cde8dedbfbfb888cd26e723a2d2
Daniel Westermann-Clark <dwc@pobox.com>
2011-09-18 21:57:25 -0700

    Sort portfolio page by project name

--- page-portfolio.php
+++ page-portfolio.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
-<?php query_posts( 'post_type=dwc_portfolio' ); ?>
+<?php
+query_posts( array(
+    'post_type' => 'dwc_portfolio',
+    'orderby' => 'title',
+    'order' => 'ASC',
+) );
+?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<?php get_template_part( 'post', 'portfolio' ); ?>
