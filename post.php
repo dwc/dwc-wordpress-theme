da7e66687f7eadf08714254aa0f296e143c1a087
Daniel Westermann-Clark <dwc@pobox.com>
2011-10-13 18:50:00 -0700

    Move article metadata into the header

--- post.php
+++ post.php
@@ -1,22 +1,21 @@
 <article>
 	<header>
 		<h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php if ( !is_page() ) : ?>
+			<ul class="metadata">
+				<li>Posted on <?php get_template_part( 'time' ); ?></li>
+				<li><a href="<?php comments_link(); ?>"><?php comments_number( 'No comments', 'One comment', '% comments' ); ?></a></li>
+				<?php if ( current_user_can( 'edit_pages' ) ) : ?>
+					<li><?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?></li>
+				<?php endif; ?>
+			</ul>
+		<?php endif; ?>
 	</header>
 
 	<?php if ( is_single() || is_page() ) : ?>
 		<?php the_content(); ?>
 	<?php else : ?>
 		<?php the_excerpt(); ?>
-	<?php endif; ?>
-
-	<?php if ( !is_page() || current_user_can( 'edit_pages' ) ) : ?>
-		<footer>
-			<?php if ( !is_page() ) : ?>
-				Posted on <?php get_template_part( 'time' ); ?> in <?php the_category( ', ' ); ?>.
-				<a href="<?php comments_link(); ?>"><?php comments_number( 'No comments', 'One comment', '% comments' ); ?></a>.
-			<?php endif; ?>
-			<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
-		</footer>
 	<?php endif; ?>
 
 	<?php comments_template( '', true ); ?>
